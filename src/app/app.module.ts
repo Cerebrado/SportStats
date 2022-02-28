37d5ed15b8d753d29c9824db18b5d6be2f6c63f2
Juan Manuel Nallar <juanmanuel.nallar@zurich.com>
2022-02-28 17:06:55 +0100

    x

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { NewMatchComponent } from './new-match/new-match.component';
@@ -9,9 +10,10 @@ import { MatchComponent } from './match/match.component';
 import { StatisticsComponent } from './statistics/statistics.component';
 import { SettingsComponent } from './settings/settings.component';
 import { LoginComponent } from './login/login.component';
+import { NewPlayerComponent } from './new-player/new-player.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, CommonModule],
+  imports: [BrowserModule, FormsModule, CommonModule, NgbModule],
   declarations: [
     AppComponent,
     NewMatchComponent,
@@ -19,6 +21,7 @@ import { LoginComponent } from './login/login.component';
     StatisticsComponent,
     SettingsComponent,
     LoginComponent,
+    NewPlayerComponent
   ],
   bootstrap: [AppComponent],
 })
