6e45485006c886bc71e02824ab02c1e31eae8f1c
Juan Manuel Nallar <juanmanuel.nallar@zurich.com>
2022-03-13 13:49:13 +0100

    x

--- src/app/match/match.component.ts
+++ src/app/match/match.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
 import { MatchService } from '../Model/match.service';
-import { Match, PlayEvent, Settings } from '../Model/model';
+import { Match, Player, PlayEvent, Settings } from '../Model/model';
 import { SettingsService } from '../Model/settings.service';
 
 @Component({
@@ -10,29 +10,48 @@ import { SettingsService } from '../Model/settings.service';
 
 })
 export class MatchComponent implements OnInit {
+
+  PlayEventsByValue: Map<number, PlayEvent[]> = new Map<number, PlayEvent[]>();
   
+
+  Match:Match;
+  StatEntry: PlayerEvent = {Player: null, PlayEvent: null};
+
   constructor(
     private matchService: MatchService,
-    private settingsService: SettingsService,
-    )  {
+    private settingsService: SettingsService) {
       this.matchService.Match.subscribe((match) =>{
         this.Match = match;
       })
       this.settingsService.Settings.subscribe((settings) =>{
-        this.Settings = settings;
+        settings.PlayEventsList.forEach(e => {
+          if(! this.PlayEventsByValue.has(e.Value))
+            this.PlayEventsByValue.set(e.Value, new Array<PlayEvent>());
+          this.PlayEventsByValue.get(e.Value).push(e);
+        });
+        // //this returns objects, not the values --> this.EventValues = [...new Map(settings.PlayEventsList.map(item =>[item['Value'], item])).values()];
+        // this.EventValues =  [...new Set(settings.PlayEventsList.map(obj => obj.Value)) ];
+        // this.PlayEvents = settings.PlayEventsList.sort((x,y) => (x.Value > y.Value)? 1 : 0).slice(0);
       })
    }
   
-  Match:Match;
-  Settings: Settings;
-  
   ngOnInit() {
-    //this.ref.detectChanges()
-    ;
+        
   }
 
-  clickEvent(ev:PlayEvent){
-    alert(ev.Short);
+  clickPlayer(player:Player){
+    alert(player.Nick);
   }
-  StatEntry: string [] = [];
+
+  clickEvent(event:PlayEvent){
+    alert(event.Short);
+  }
+
+}
+
+
+export interface PlayerEvent{
+  Player:Player | null;
+  PlayEvent: PlayEvent | null;
+  
 }
